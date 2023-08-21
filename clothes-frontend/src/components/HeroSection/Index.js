ff240a214768e310ea4bab0404962e2a4a5f4f71
rishabhtak <rishabhtak93@gmail.com>
2023-08-21 20:08:05 +0530

    added herosection in home page

--- clothes-frontend/src/components/HeroSection/Index.js
+++ clothes-frontend/src/components/HeroSection/Index.js
@@ -1,41 +1,86 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
     <div className="px-5 py-16 mx-auto flex flex-wrap">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
         <div className="col-span-8 relative h-full w-full">
+          <div className="text-center absolute w-full h-full bg-black/25 grid content-center">
+            <p className="leading-relaxed text-white uppercase">
+              sale upto 40% off
+            </p>
+            <h1 className="text-2xl md:text-6xl text-white font-bold font-oswald mb-4 capitalize">
+              hexashop online store
+            </h1>
+            <div>
+              <Link
+                href="/"
+                className="mt-3 text-white inline-flex items-center capitalize border border-white px-4 md:px-6 py-1 md:py-2 transition ease-in-out hover:bg-white hover:text-black duration-300"
+              >
+                shop now
+              </Link>
+            </div>
+          </div>
           <Image
             src="/offers-banner.jpg"
             alt="offers banner image"
             height={1080}
             width={1920}
-            className="h-full w-full object-cover"
+            className="w-full object-cover h-full object-center inset-0"
           />
-          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/25">
-            <h1 className="text-white text-6xl">hexashop</h1>
-            <span>hello how are you</span>
-          </div>
         </div>
-        <div className="col-span-8 lg:col-span-4">
+        <div className="col-span-8 md:col-span-4">
           <div className="flex flex-col gap-4">
-            <div>
+            <div className="relative h-full w-full">
+              <div className="text-center absolute w-full h-full bg-black/25 grid content-center">
+                <p className="text-2xl md:text-3xl text-white font-bold font-oswald mb-4 capitalize">
+                  Women
+                </p>
+                <p className="leading-relaxed text-white capitalize">
+                  Best Clothes For Women
+                </p>
+                <div>
+                  <Link
+                    href="/"
+                    className="mt-3 text-white inline-flex items-center capitalize border border-white px-4 md:px-6 py-1 md:py-2 transition ease-in-out hover:bg-white hover:text-black duration-300"
+                  >
+                    shop now
+                  </Link>
+                </div>
+              </div>
               <Image
                 src="/women-banner.jpg"
                 alt="women banner image"
                 height={1080}
                 width={1920}
-                className="w-full object-cover h-full object-center"
+                className="w-full object-cover h-full object-center inset-0"
               />
             </div>
-            <div>
+            <div className="relative h-full w-full">
+              <div className="text-center absolute w-full h-full bg-black/25 grid content-center">
+                <p className="text-2xl md:text-3xl text-white font-bold font-oswald mb-4 capitalize">
+                  men
+                </p>
+                <p className="leading-relaxed text-white capitalize">
+                  Best Clothes For men
+                </p>
+                <div>
+                  <Link
+                    href="/"
+                    className="mt-3 text-white inline-flex items-center capitalize border border-white px-4 md:px-6 py-1 md:py-2 transition ease-in-out hover:bg-white hover:text-black duration-300"
+                  >
+                    shop now
+                  </Link>
+                </div>
+              </div>
               <Image
                 src="/men-banner.jpg"
-                alt="offers banner image"
+                alt="men banner image"
                 height={1080}
                 width={1920}
-                className="w-full object-cover h-full object-center"
+                className="w-full object-cover h-full object-center inset-0"
               />
             </div>
           </div>
